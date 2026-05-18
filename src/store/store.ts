27462f1701f6ce27a9196c1f3a32de937d88b01d
Harihara8209 <h37904139@gmail.com>
2026-05-18 12:02:37 +0530

    added changes

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,6 +3,6 @@ import { authSlice } from "./slices/authSlice";
 
 export const store=configureStore({
     reducer:authSlice.reducer,
-    
+        
 })
 export type RootState = ReturnType<typeof store.getState>
